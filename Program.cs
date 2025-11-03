70dacfc0e22068c9b0d59d5d06102a7262b07a8e
GRIGORIO\kim-g <kim-g@yandex.ru>
2025-11-03 12:24:11 +0500

    Показали работу оператора if

--- Program.cs
+++ Program.cs
@@ -1,5 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Как Вас зовут?");
-string UserName;
+string? UserName;
 UserName = Console.ReadLine();
-Console.WriteLine("Привет, " + UserName);
+
+if (UserName == "" || UserName == null)
+{
+    Console.WriteLine("Жалко, что Вы не представились. Будем хвать Вас \"Аноним\"");
+    UserName = "Аноним";
+}
+
+string Text = "Сердечно поздравляем {Name} с награждением государственной наградой - орденом Дружбы за заслуги в научной деятельности и многолетнюю добросовестную работу. Желаем {Name} новых творческих успехов! Коллектив ИОС УрО РАН поздравляет {Name}";
+string TextWithName = Text.Replace("{Name}", UserName);
+
+Console.WriteLine(TextWithName);
