8292c79a20e622bb346e2f3a1673d9f8f4ae3f61
GRIGORIO\kim-g <kim-g@yandex.ru>
2025-11-03 11:47:03 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Как Вас зовут?");
+string UserName;
+UserName = Console.ReadLine();
+Console.WriteLine("Привет, " + UserName);
