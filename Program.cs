67488fc81ad4aa01f6894a4b0b0e1af3b1af3960
GRIGORIO\kim-g <kim-g@yandex.ru>
2025-11-10 11:24:04 +0500

    Добавили переменную для if

--- Program.cs
+++ Program.cs
@@ -1,12 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Как Вас зовут?");
 string? UserName;
-UserName = Console.ReadLine();
 
-if (UserName == "" || UserName == null)
+//UserName = Console.ReadLine();
+//bool IsNameCorrect = UserName == "" && UserName == null;
+bool IsNameCorrect = 2 != 2;
+Console.WriteLine(IsNameCorrect);
+if (IsNameCorrect)
 {
     Console.WriteLine("Жалко, что Вы не представились. Будем хвать Вас \"Аноним\"");
     UserName = "Аноним";
+}
+else
+{
+    Console.WriteLine($"Приятно познакомиться, {UserName}!");
 }
 
 string Text = "Сердечно поздравляем {Name} с награждением государственной наградой - орденом Дружбы за заслуги в научной деятельности и многолетнюю добросовестную работу. Желаем {Name} новых творческих успехов! Коллектив ИОС УрО РАН поздравляет {Name}";
